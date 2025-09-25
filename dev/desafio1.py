69c9ce84a82e53ebf34cdff8def0c246de361fee
lucioamarante <lucio59241446@edu.senac.br>
2025-09-25 19:33:05 -0300

    ok

---
+++ dev/desafio1.py
@@ -0,0 +1,16 @@
+"""comentário de multiplas linhas
+"""
+# comentário de uma linha
+
+"""
+Para rodar esse programa
+Crie uma venv (ambiente virtual)
+
+python -m venv venv
+
+primeira venv obrigatória
+segunda venv (voce decide)
+"""
+
+# saída de dados
+print("Hello World")
